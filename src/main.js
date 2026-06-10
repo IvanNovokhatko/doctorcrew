ac90dd16fa666df10bc251168c48613ddb6fda42
Novokhatko Ivan <kentvanek80@gmail.com>
2026-06-10 23:46:02 +0300

    adds the base structure

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header.js';
+import './js/pets-list.js';
+import './js/about-us.js';
+import './js/faq.js';
+import './js/success-stories.js';
+import './js/animal-details-modal.js';
+import './js/order-modal.js';
