ed552e2f37800920b29f9e42e545901ee56d7122
Andrii Pavelko <andreypavelko27@gmail.com>
2026-06-14 19:30:43 +0300

    update m and t

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,8 +1,8 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import Accordion from 'accordion-js';
+// // import 'accordion-js/dist/accordion.min.css';
 
- new Accordion('.accordion-container', {
-  duration: 400,
-  showMultiple: false,
-  openOnInit: [0],
-});
+//  new Accordion('.accordion-container', {
+//   duration: 400,
+//   showMultiple: false,
+//   openOnInit: [0],
+// });
