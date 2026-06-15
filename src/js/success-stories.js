275bf50a9cf4f35342e43909e963903f6ab0bd43
shinobu_kage <maksim.lub@icloud.com>
2026-06-16 00:33:20 +0300

    update hero btn

--- src/js/success-stories.js
+++ src/js/success-stories.js
@@ -1,15 +1,15 @@
 // Star Section by 
-//* function getStarsHTML(rate) {
-//*    let starsMarkup = '';
-//*    for (let i = 1; i <= 5; i++) {
-//*        if (rate >= i) {
-//*            starsMarkup += `<li class="star"><svg class="star-icon"><use href="../svg/img.svg#star-filled"></use></svg></li>`;
-//*        } else if (rate >= i - 0.5) {
-//*            starsMarkup += `<li class="star"><svg class="star-icon"><use href="../svg/img.svg#star-half"></use></svg></li>`;
-//*        } else {
-//*            starsMarkup += `<li class="star"><svg class="star-icon"><use href="../svg/img.svg#star-outline"></use></svg></li>`;
-//*        }
-//*    }
-//*    return `<ul class="feedback-stars-list">${starsMarkup}</ul>`;
-//* }
+//  function getStarsHTML(rate) {
+//     let starsMarkup = '';
+//     for (let i = 1; i <= 5; i++) {
+//         if (rate >= i) {
+//             starsMarkup += `<li class="star"><svg class="star-icon"><use href="../svg/img.svg#star-filled"></use></svg></li>`;
+//         } else if (rate >= i - 0.5) {
+//             starsMarkup += `<li class="star"><svg class="star-icon"><use href="../svg/img.svg#star-half"></use></svg></li>`;
+//         } else {
+//             starsMarkup += `<li class="star"><svg class="star-icon"><use href="../svg/img.svg#star-outline"></use></svg></li>`;
+//         }
+//     }
+//     return `<ul class="feedback-stars-list">${starsMarkup}</ul>`;
+//  }
 
