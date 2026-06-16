7b3f43d2ac27bef32151099164bef2e0aa929fd6
Andrii Pavelko <andreypavelko27@gmail.com>
2026-06-16 15:14:08 +0300

    update FAQ.JS

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,29 +1,13 @@
 import Accordion from 'accordion-js';
 
-
-
-
-function switchIcons() {
-  document.querySelectorAll('.ac').forEach(item => {
-    const icon = item.querySelector('.faq-icon use');
-
-    if (item.classList.contains('is-active')) {
-      icon.setAttribute('href', '/img/icons.svg#close');
-    } else {
-      icon.setAttribute('href', '/img/icons.svg#add');
-    }
-  });
-}
-
-document.querySelectorAll('.ac').forEach(item => {
-  item.addEventListener('click', () => {
-    setTimeout(updateIcons, 0);
-  });
-});
-
-switchIcons();
-
 new Accordion('.accordion-container', {
-  duration: 400,
-  showMultiple: false,
+  onOpen(item) {
+    item.querySelector('.faq-icon use')
+      .setAttribute('href', '/img/icons.svg#close');
+  },
+
+  onClose(item) {
+    item.querySelector('.faq-icon use')
+      .setAttribute('href', '/img/icons.svg#add');
+  }
 });
